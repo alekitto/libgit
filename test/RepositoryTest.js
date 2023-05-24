05f52aba005b70b3ef42d0970242f404ccc669c5
Alessandro Chitolina <alekitto@gmail.com>
2023-05-25 00:01:57 +0200

    fix(test): fixed test execution on aarch64-unknown-linux-musl

--- test/RepositoryTest.js
+++ test/RepositoryTest.js
@@ -1,38 +1,50 @@
 import { Repository, RepositoryState } from '../index';
-import { mkdtempSync } from 'node:fs';
+import { randomBytes } from 'node:crypto';
 import { tmpdir } from 'node:os';
 
+const Filesystem = Jymfony.Component.Filesystem.Filesystem;
 const TestCase = Jymfony.Component.Testing.Framework.TestCase;
 
+const fs = new Filesystem();
+
 export default class RepositoryTest extends TestCase {
+    _tmpDirName;
+
+    async beforeEach() {
+        this._tmpDirName = tmpdir() + '/' + randomBytes(5).toString('base64').replace(/[^0-9a-z]/i, '-');
+        await fs.mkdir(this._tmpDirName);
+    }
+
+    async afterEach() {
+        await fs.remove(this._tmpDirName);
+    }
+
     testRepositoryInit() {
-        const repo = Repository.init(mkdtempSync(tmpdir() + '/libgit-'));
+        const repo = Repository.init(this._tmpDirName);
         __self.assertFalse(repo.isBare());
         __self.assertTrue(repo.isEmpty());
     }
 
     testRepositoryInitBare() {
-        const repo = Repository.init(mkdtempSync(tmpdir() + '/libgit-'), true);
+        const repo = Repository.init(this._tmpDirName, true);
         __self.assertTrue(repo.isBare());
         __self.assertTrue(repo.isEmpty());
         __self.assertNull(repo.namespace());
     }
 
     testRepositoryOpen() {
-        const path = mkdtempSync(tmpdir() + '/libgit-');
-        Repository.init(path);
+        Repository.init(this._tmpDirName);
 
-        const repo = Repository.open(path);
+        const repo = Repository.open(this._tmpDirName);
         __self.assertFalse(repo.isBare());
         __self.assertTrue(repo.isEmpty());
         __self.assertEquals(RepositoryState.Clean, repo.state());
     }
 
     testRepositoryOpenBare() {
-        const path = mkdtempSync(tmpdir() + '/libgit-');
-        Repository.init(path, true);
+        Repository.init(this._tmpDirName, true);
 
-        const repo = Repository.open(path);
+        const repo = Repository.open(this._tmpDirName);
         __self.assertTrue(repo.isBare());
         __self.assertTrue(repo.isEmpty());
         __self.assertEquals(RepositoryState.Clean, repo.state());
